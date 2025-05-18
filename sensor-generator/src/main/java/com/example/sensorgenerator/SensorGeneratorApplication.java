6f518b5b1be960ea08b6432f9751c26fc0b50e58
DenysSoldatenko <soldatenko911@gmail.com>
2025-05-18 19:15:00 +0300

    feat: add initial implementation of sensor-generator service

---
+++ sensor-generator/src/main/java/com/example/sensorgenerator/SensorGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.sensorgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorGeneratorApplication.class, args);
+	}
+
+}
---
+++ sensor-generator/src/test/java/com/example/sensorgenerator/SensorGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensorgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
