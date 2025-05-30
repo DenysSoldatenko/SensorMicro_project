29d5c269be22ca1508aab1c7edfe4da3067133d8
DenysSoldatenko <soldatenko911@gmail.com>
2025-05-30 20:50:00 +0300

    feat: add initial implementation of sensor-analyzer service

---
+++ sensor-analyzer/src/main/java/com/example/sensoranalyzer/SensorAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sensoranalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorAnalyzerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SensorAnalyzerApplication.class, args);
+  }
+
+}
---
+++ sensor-analyzer/src/test/java/com/example/sensoranalyzer/SensorAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensoranalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorAnalyzerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
