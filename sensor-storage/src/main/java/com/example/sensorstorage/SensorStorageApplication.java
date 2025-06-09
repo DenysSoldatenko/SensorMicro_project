54ef84d7f5a890b913400830422faf3c053329d5
DenysSoldatenko <soldatenko911@gmail.com>
2025-06-09 19:15:00 +0300

    feat: add initial implementation of sensor-storage service

---
+++ sensor-storage/src/main/java/com/example/sensorstorage/SensorStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.sensorstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorStorageApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SensorStorageApplication.class, args);
+  }
+
+}
---
+++ sensor-storage/src/test/java/com/example/sensorstorage/SensorStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensorstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorStorageApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
